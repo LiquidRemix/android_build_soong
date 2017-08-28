aea0de5e77a959ff92928ac15d14c51fda049d53
ezio84 <brabus84@gmail.com>
2017-08-28 19:51:57 +0200

    Kati: use one less cpu

--- cmd/multiproduct_kati/main.go
+++ cmd/multiproduct_kati/main.go
@@ -34,7 +34,7 @@ import (
 
 // We default to number of cpus.
 func detectNumJobs() int {
-	return runtime.NumCPU()
+	return (runtime.NumCPU() - 1)
 }
 
 var numJobs = flag.Int("j", detectNumJobs(), "number of parallel kati jobs")
